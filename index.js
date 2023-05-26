fd7f4546b43ee39ac62b70a72039f70d4abc6990
Deeti <deetigupta8@gmail.com>
2023-05-26 17:25:39 +0530

    basic add and del

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("champs")
+console.log("start...");
+const parentEl = document.getElementById("sec3");
+const publishBtn = document.getElementById("publishBtn");
+const inputEl = document.getElementById("textarea");
+const postList = document.getElementsByClassName("postList");
+
+publishBtn.addEventListener("click", function () {
+    let message = ""
+    let childEl = document.createElement("p")
+    childEl.classList.add("postList")
+    message = inputEl.value
+    childEl.innerText = message
+    parentEl.appendChild(childEl)
+    inputEl.value = "";
+})
+
+parentEl.addEventListener("dblclick", function (e) {
+    for (let i = 0; i < postList.length; i++) {
+        if (e.target.innerText === postList[i].innerText) {
+            parentEl.removeChild(postList[i])
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+//console.log(inputEl.value)
+
